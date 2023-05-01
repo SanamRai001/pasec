1db95cfa836743f0d3f81c66883b871abe91c51d
ssuroj <surojsapkota1@gmail.com>
2023-05-01 13:39:20 +0545

    Added autofocus and resolved some minor issue

--- js/script.js
+++ js/script.js
@@ -14,6 +14,7 @@ var finalPassword = "";
 // Get DOM elements
 const result = document.getElementById('result');
 const lengthSlider = document.getElementById('slider');
+const LengthText = document.getElementById('length');
 const passPhraseInput = document.getElementById('passphrase');
 const uppercaseCheckbox = document.getElementById('uppercase');
 const lowercaseCheckbox = document.getElementById('lowercase');
@@ -63,7 +64,7 @@ function passwordGenerator(length, uppercase, lowercase, numbers, symbols, passp
 
 // Function to update password in real-time
 function updatePassword() {
-  const length = lengthSlider.value;
+  const length = LengthText.value;
   const uppercase = uppercaseCheckbox.checked;
   const lowercase = lowercaseCheckbox.checked;
   const numbers = numbersCheckbox.checked;
@@ -72,8 +73,8 @@ function updatePassword() {
   
   var password = passwordGenerator(length, uppercase, lowercase, numbers, symbols, passphrase);
   finalPassword = password;
-  if (password.length > 16) {
-    password = password.substring(0, 16) + ' ...';
+  if (password.length > 20) {
+    password = password.substring(0, 20) + ' ...';
   }
   // Update the text content of the result element
   resultText.textContent = password;
@@ -82,58 +83,10 @@ function updatePassword() {
 
 
 // Attach event listeners
+LengthText.addEventListener('input', updatePassword);
 lengthSlider.addEventListener('input', updatePassword);
 uppercaseCheckbox.addEventListener('change', updatePassword);
 lowercaseCheckbox.addEventListener('change', updatePassword);
 numbersCheckbox.addEventListener('change', updatePassword);
 symbolsCheckbox.addEventListener('change', updatePassword);
 passPhraseInput.addEventListener('input', updatePassword);
-
-// clipboardButton.addEventListener('click', (password) => {
-//   // Create a temporary textarea element to hold the password
-//   const textarea = document.createElement('textarea');
-//   textarea.value = resultText.innerText;
-//   document.body.appendChild(textarea);
-  
-//   // Select the text and copy it to the clipboard
-//   textarea.select();
-//   document.execCommand('copy');
-  
-//   // Remove the temporary textarea
-//   document.body.removeChild(textarea);
-//   resultText.innerHTML = "<p style='color:red;'> Copied!</p>";
-//   // Show a confirmation message to the user
-//   setTimeout(() => {
-//     resultText.textContent = "";
-//   }, 1000)
-// });
-// Function to copy password to clipboard
-// function copyIt() {
-//   passwordOutput.select();
-//   navigator.clipboard.writeText(passwordOutput.value);
-// }
-
-// // Attach event listener to copy button
-// copyButton.addEventListener('click', copyIt);
-
-// jubrish.value = generatejubrish(lengthSlider.value);
-// // program to generate random strings
-
-// // declare all characters
-
-// function generatejubrish(length) {
-//     const jub_char ='!@#$%^&*()_+=-ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-//     let result = ' ';
-//     const charactersLength = jub_char.length;
-//     for ( let i = 0; i < length; i++ ) {
-//         result += jub_char.charAt(Math.floor(Math.random() * charactersLength));
-//     }
-
-//     return result;
-// }
-
-
-// function copyJubrish() {
-//     const jubrishText = document.getElementById("jubrish").innerText;
-//     navigator.clipboard.writeText(jubrishText);
-//   }
